d323c34c80077f42590223004ff7ecc5c459f235
random-debug <85326677+random-debug@users.noreply.github.com>
2022-12-03 14:52:11 -0800

    Changed Queue to LinkedQueue with java library

--- LinkedQueue.java
+++ LinkedQueue.java
@@ -1,63 +1,30 @@
-import java.util.NoSuchElementException;
-
+import java.util.Queue;
+import java.util.LinkedList;
 public class LinkedQueue<T> implements QueueInterface<T> {
 
-    private Node firstNode;
-    private Node lastNode;
+    private Queue<T> queue;
 
     public LinkedQueue() {
-        firstNode = null;
-        lastNode = null;
+        queue = new LinkedList<>();
     }
 
     public void enqueue(T newEntry) {
-        Node newNode = new Node(newEntry);
-
-        if (isEmpty())
-            firstNode = newNode;
-        else
-            lastNode.next = newNode;
+        queue.add(newEntry);
     }
 
     public T dequeue() {
-        T front = getFront();
-        firstNode.data = null;
-        firstNode = firstNode.next;
-
-        if (firstNode == null)
-            lastNode = null;
-        return front;
-
+        return queue.poll();
     }
 
     public T getFront() {
-        if (isEmpty())
-            throw new NoSuchElementException();
-        else
-            return firstNode.data;
+        return queue.peek();
     }
 
     public boolean isEmpty() {
-        return (firstNode == null) && (lastNode == null);
+        return queue.isEmpty();
     }
 
-    public void clear() { // Will run out of memory. Not cleared properly
-        firstNode = null;
-        lastNode = null;
+    public void clear() {
+        queue.clear();
     }
-
-    private class Node {
-        private T data;
-        private Node next;
-
-        private Node(T dataPortion) {
-            this(dataPortion, null);
-        }
-
-        private Node(T dataPortion, Node nextNode) {
-            data = dataPortion;
-            next = nextNode;
-        }
-    }
-
 }
